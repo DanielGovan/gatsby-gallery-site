ddeb8da5bb59184530939e4488107871f8e75e23
Dan Govan <danielgovan@gmail.com>
2021-01-12 10:08:13 +0000

    Hiding gallery headers

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -2,21 +2,21 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import Para from "../actualComponents/Para"
-import { InnerWrap, Header } from "../components/LayoutElements"
+// import Para from "../actualComponents/Para"
+// import { InnerWrap, Header } from "../components/LayoutElements"
 import Gallery from "../components/Gallery"
 
 const Testimonials = () => {
   return (
     <Layout>
       <SEO title="Portrait Gallery" />
-      <InnerWrap>
+      {/* <InnerWrap>
         <Header>Portrait Gallery</Header>
         <Para>
           Here's everyone I've ever shot! Both for easy reference, and as an
           antidote for my imposter syndrome.
         </Para>
-      </InnerWrap>
+      </InnerWrap> */}
       <Gallery />
     </Layout>
   )
