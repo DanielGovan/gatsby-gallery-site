952dc33ce2cc44da173b85430de4ee6f8fda284b
Dan Govan <danielgovan@gmail.com>
2021-01-24 00:02:28 +0000

    Updated testimonial images

---
+++ src/const/testimonials.js
@@ -0,0 +1,126 @@
+export const TESTIMONIAL_COPY = [
+  {
+    name: "andreas",
+    quote:
+      "Was lovely, loved the result and was super comfortable. Round 2 with bubbles pls!",
+    image: "andreas.jpg",
+  },
+  {
+    name: "andy",
+    quote: "Professional but super-",
+    image: "andy.jpg",
+  },
+  {
+    name: "Charlie",
+    quote:
+      "Great experience! I had never done anything like this and you made me feed v at ease.",
+    image: "charlie.jpg",
+  },
+  {
+    name: "Chirag",
+    quote:
+      "Dan made me feel super comfortable and put my nerves at ease straight away, made me feel super confident in the shoot and really enjoyable",
+    image: "chirag.jpg",
+  },
+  {
+    name: "chris",
+    quote:
+      "Brilliant! A calm and comfy environment, and the shots were amazing",
+    image: "chris.jpg",
+  },
+  {
+    name: "Daniel",
+    quote:
+      "Such an amazing experience! Fun, creative and humorous. Work has only gotten better too.",
+    image: "danb.jpg",
+  },
+  {
+    name: "Dan",
+    quote:
+      "Couldn;t have felt more at ease! Great experience to really see your body as beautiful.",
+    image: "danf.jpg",
+  },
+  {
+    name: "Diogo",
+    quote:
+      "Dan made me feel comfortable despite how insecure I am about my physical appearance.",
+    image: "diago.jpg",
+  },
+  {
+    name: "jake",
+    quote:
+      "He was lovely and put me at ease even htough I had very little experience. 5 stars.",
+    image: "jake.jpg",
+  },
+  {
+    name: "John",
+    quote:
+      "It was fun and relaxed and playful, and lead to images that have continued to prove popular with editors, publicists and fans!",
+    image: "johneast.jpg",
+  },
+  {
+    name: "Manuel",
+    quote:
+      "I've never felt more comfortable! a beautiful soul and a great artist",
+    image: "manuel.jpg",
+  },
+  {
+    name: "Marcus",
+    quote:
+      "Having never done this before I was pretty nervous but dan made everytying comfortable and fun.",
+    image: "marcus.jpg",
+  },
+  {
+    name: "Matt",
+    quote:
+      "It was a total joy and I'm thrilled with the restults - which reminds me to share some more again!",
+    image: "matt.jpg",
+  },
+  {
+    name: "Owen",
+    quote:
+      "Creative, credible and caring Dan made me feel welcome and wonderful during our shoot.",
+    image: "owen.jpg",
+  },
+  {
+    name: "Paud",
+    quote:
+      "Dan is inquisitive and reassuring, I'm a firm believer that the best work comes from the kindest environments, which must be why his photos are so beautiful",
+    image: "paud.jpg",
+  },
+  {
+    name: "Reggie",
+    quote:
+      "Tons of fun, the results were great. Meta great photographer and got naked!",
+    image: "reggie.jpg",
+  },
+  {
+    name: "Sam",
+    quote: "Super comfortable environment and such an open creative space.",
+    image: "sam.jpg",
+  },
+  {
+    name: "Seb",
+    quote:
+      "One of the most chilled out and most comfortable and entertaining shoots I've done, Dan mastered making perfect photos and being a perfect human, I WANT MORE!",
+    image: "seb.jpg",
+  },
+  {
+    name: "stella",
+    quote:
+      "Shooting with Dan was the best experience. He was professional and fun and so talented.",
+    image: "stella.jpg",
+  },
+  {
+    name: "Tom",
+    quote:
+      "Such a wonderful and comfortable experience! I've never felt so safe and honestly couldn't recommend shooting with Dan enough! I've found the experience so fun and thrilling, especially when I'm not used to being in front of the camera!",
+    image: "tomr.jpg",
+  },
+  {
+    name: "Toni",
+    quote:
+      "I was lit by Dan and it was a very easy, friendly and fun experience!",
+    image: "toni.jpg",
+  },
+]
